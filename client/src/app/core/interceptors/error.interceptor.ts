0fc5fe6559760fa8726b370aa8939754aa37e55c
ChandruSrini <chandrusrini95@gmail.com>
2021-12-13 11:11:34 +0530

    end of section 11

---
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { NavigationExtras, Router } from '@angular/router';
+import { catchError } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+  constructor(private router: Router, private toastr: ToastrService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+
+    return next.handle(request).pipe(
+      catchError(error =>{
+        if(error){
+          if(error.status === 404){
+            this.router.navigateByUrl('/not-found')
+          }
+
+          if(error.status === 500){
+            //error.error is the error object we return api
+            const navigationExtras: NavigationExtras = {state: {error: error.error}}
+            this.router.navigateByUrl('/server-error', navigationExtras)
+          }
+
+          //to handle 400 and 404 validation error we make use of error.error(from api) object
+          if(error.status === 400){
+            //404 validation error
+            if(error.error.errors){
+              throw error.error
+            } else{
+              this.toastr.error(error.error.message, error.error.statusCode)
+            }
+          }
+
+          if(error.status === 401){
+            this.toastr.error(error.error.message, error.error.statusCode)
+          }
+        }
+        return throwError(error)
+      })
+    )
+  }
+}
